c9adb0608767828ed4f87a0ac8a427ed84928979
Ali <thexali123@gmail.com>
2023-07-26 17:24:40 +0300

    Task.js updated

---
+++ src/components/Task.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Task() {
+    return (
+        <div className="container">
+            <form>
+                <input type="text">
+                    
+                </input>
+            </form>
+        </div>
+    )
+}
+export default Task;
